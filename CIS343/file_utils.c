4381102407922127f201d3d3bc4f8189da8d5812
Jarred Parr <jparr721@gmail.com>
2018-09-07 11:51:26 -0400

    working...

--- CIS343/file_utils.c
+++ CIS343/file_utils.c
@@ -1,9 +1,27 @@
-#include "util.h"
+#include "file_utils.h"
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int read_file(char* filename, char** buffer) {
+  FILE* fp;
+  char* line = NULL;
+  size_t len = 0;
+  ssize_t read;
+
+  fp = fopen(filename, "r");
+  if (fp == NULL) {
+    fprintf(stderr, "BOYYYY THAT AINT A FILE, YA'LL");
+    exit(EXIT_FAILURE);
+  }
+
+  while ((read = getline(&line, &len, fp)) != -1) {
+    strcat(line, buffer);
+  }
 
 }
 
-int write_file(char* filenamem char* buffer, int size) {
+int write_file(char* filename, char* buffer, int size) {
 
 }
--- CIS343/reverse.c
+++ CIS343/reverse.c
@@ -1,9 +1,22 @@
 #include "file_utils.c"
+#include <errno.h>
+#include <stdio.h>
 
-int reverse() {
+int reverse(char* file_1, char* file_2) {
+  char* buffer = malloc(sizeof(char)*2048);
+  char* new_file = malloc(sizeof(char)*2048);
 
+  if (read_file(file_1, &buffer) != 0) {
+    fprinf(stderr, "BOYYYY WE CAN'T READ INTO THIS BUFFER");
+    return errno;
+  }
+
+  if (write_file(file_2, new_file, sizeof(buffer))) {
+
+  }
 }
 
-int main(void) {
-  reverse();
+int main(int argc, char** argv) {
+  char* file_1 = argv[1];
+  char* file_2 = argv[2];
 }
