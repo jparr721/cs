172b1aea8996aa22fed7a57a2c18cdba129d7c52
Jarred Parr <jparr721@gmail.com>
2018-09-12 21:59:01 -0400

    code now transfers as intended

--- CIS457/lab_1/client.c
+++ CIS457/lab_1/client.c
@@ -55,7 +55,7 @@ int main(void) {
 
   FILE* fp = fopen("copy.txt", "w");
 
-  fwrite(buffer, sizeof(char), strlen(buffer) + 1, fp);
+  fwrite(buffer, sizeof(char), strlen(buffer), fp);
 
   return 0;
 }
