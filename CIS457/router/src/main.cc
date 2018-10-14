3332efa6e59b23f5bd8905ab34bc12520257205d
Jarred Parr <jparr721@gmail.com>
2018-10-13 22:28:40 -0400

    invoke the start funciton

--- CIS457/router/src/main.cc
+++ CIS457/router/src/main.cc
@@ -12,5 +12,13 @@ int main(int argc, char** argv) {
   if (argc < 2) {
     std::cout << "usage: router routing_table" << std::endl;
   }
+  std::string routing_table = argv[1];
   router::Router r;
+  int router = r.Start(routing_table);
+  if (router < 0) {
+    std::cerr << "Failed to intialize routing interface..." << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  return EXIT_SUCCESS;
 }
