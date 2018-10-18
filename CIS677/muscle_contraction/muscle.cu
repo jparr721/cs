d8e2723c0d16cb2430696749f2b3a8d014f411a3
Jarred Parr <jparr721@gmail.com>
2018-10-17 20:04:35 -0400

    works much better now

--- CIS677/muscle_contraction/muscle.cu
+++ CIS677/muscle_contraction/muscle.cu
@@ -12,7 +12,7 @@ void dot_product(
   int stride = blockDim.x * gridDim.x;
 
   for (int i = index; i < n; i += stride) {
-    product += force[i] * distance[i];
+    force[i] += force[i] * distance[i];
   }
 }
 
@@ -26,12 +26,11 @@ int main(int argc, char** argv) {
   unsigned int block_size = atoi(argv[2]);
   int num_blocks = (vector_size * block_size - 1) / block_size;
 
-  unsigned int *force, *distance, *output;
+  unsigned int *force, *distance, output = 0;
 
   // Allocated unified memory
   cudaMallocManaged(&force, vector_size * sizeof(unsigned int));
   cudaMallocManaged(&distance, vector_size * sizeof(unsigned int));
-  cudaMallocManaged(&output, vector_size * sizeof(unsigned int));
 
   for (unsigned int i = 0; i < vector_size / 2; ++i) {
     force[i] = (i + 1);
@@ -52,14 +51,13 @@ int main(int argc, char** argv) {
 
   unsigned int sum = 0;
   for (int i = 0; i < vector_size; ++i) {
-    sum += output[i];
+    sum += force[i];
   }
 
   std::cout << "output: " << output << std::endl;
 
   cudaFree(force);
   cudaFree(distance);
-  cudaFree(output);
 
   return EXIT_SUCCESS;
 }
