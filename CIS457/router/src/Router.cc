15b85204f21259c704e3bf050a1403e7194c248e
Jarred Parr <jparr721@gmail.com>
2018-10-20 20:05:24 -0400

    add router two forwarding

--- CIS457/router/src/Router.cc
+++ CIS457/router/src/Router.cc
@@ -233,6 +233,18 @@ namespace router {
                 continue;
               } else if (packet_ip.substr(0, 4).compare("10.1")) {
                 std::cout << "This packet belongs to router two, forwarding" << std::endl;
+                std::memcpy(send_buffer, buf, 1500);
+                icmp_outgoing = (ICMPHeader*) (send_buffer + sizeof(ether_header) + sizeof(IPHeader));
+                icmp_outgoing->type = 0;
+                icmp_outgoing->checksum = 0;
+                icmp_outgoing->checksum = checksum(reinterpret_cast<unsigned char*>(icmp_outgoing), (1500 - sizeof(ether_header) - sizeof(IPHeader)));
+
+                // Copy data into the ip header
+                ip_outgoing = (IPHeader*) (send_buffer + sizeof(ether_header));
+                std::memcpy(ip_outgoing->src_ip, ip_incoming->dest_ip, 4);
+                std::memcpy(ip_outgoing->dest_ip, router_two_address.c_str(), 4);
+                send(interfaces[i], send_buffer, n, 0);
+                continue;
               }
 
               std::memcpy(send_buffer, buf, 1500);
