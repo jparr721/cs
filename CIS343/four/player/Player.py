322e60f4175cb98f7e2e68cf21ec6ee0cee6c449
Jarred Parr <jparr721@gmail.com>
2018-11-02 14:52:48 -0400

    laying out architecture

--- CIS343/four/player/Player.py
+++ CIS343/four/player/Player.py
@@ -4,6 +4,7 @@ import pyaudio
 import wave
 import time
 
+
 class Player:
     def __init__(self):
         self.currentSong = "Nothing playing."
@@ -14,7 +15,7 @@ class Player:
         return self.currentSong
 
     def pause(self):
-        if self.paused == False:
+        if self.paused is False:
             self.paused = True
             self.stream.stop_stream()
         else:
@@ -30,11 +31,12 @@ class Player:
         self.p = pyaudio.PyAudio()
 
         # open self.stream using callback (3)
-        self.stream = self.p.open(format=self.p.get_format_from_width(self.wf.getsampwidth()),
-                channels=self.wf.getnchannels(),
-                rate=self.wf.getframerate(),
-                output=True,
-                stream_callback=self.callback)
+        self.stream = self.p.open(format=self.p.get_format_from_width(
+                                  self.wf.getsampwidth()),
+                                  channels=self.wf.getnchannels(),
+                                  rate=self.wf.getframerate(),
+                                  output=True,
+                                  stream_callback=self.callback)
 
         # start the self.stream (4)
         self.stream.start_stream()
@@ -44,9 +46,8 @@ class Player:
         self.stream.close()
         self.wf.close()
 
-        self.p.terminate() 
+        self.p.terminate()
 
     def callback(self, in_data, frame_count, time_info, status):
         data = self.wf.readframes(frame_count)
         return (data, pyaudio.paContinue)
-
