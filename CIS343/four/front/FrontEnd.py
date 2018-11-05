3597878e9deabf64b2e7cc70f478e574baedca6e
Jarred Parr <jparr721@gmail.com>
2018-11-05 08:20:43 -0500

    fix library functionality

--- CIS343/four/front/FrontEnd.py
+++ CIS343/four/front/FrontEnd.py
@@ -1,24 +1,25 @@
 import curses
 import curses.textpad
 import os
+import sys
 
 
 class FrontEnd:
 
     def __init__(self, player):
         self.player = player
-        self.player.play('media/cello.wav')
         curses.wrapper(self.menu)
         self.root_directory_files = []
+        self.song_root = sys.argv[1]
 
     def menu(self, args):
         self.stdscr = curses.initscr()
         self.stdscr.border()
-        self.stdscr.addstr(0, 0, "cli-audio", curses.A_REVERSE)
-        self.stdscr.addstr(5, 10, "c - Change current song")
-        self.stdscr.addstr(6, 10, "p - Play/Pause")
-        self.stdscr.addstr(7, 10, "l - Library")
-        self.stdscr.addstr(9, 10, "ESC - Quit")
+        self.stdscr.addstr(0, 0, 'cli-audio', curses.A_REVERSE)
+        self.stdscr.addstr(5, 10, 'c - Change current song')
+        self.stdscr.addstr(6, 10, 'p - Play/Pause')
+        self.stdscr.addstr(7, 10, 'l - Library')
+        self.stdscr.addstr(9, 10, 'ESC - Quit')
         self.update_song()
         self.stdscr.refresh()
         while True:
@@ -33,12 +34,12 @@ class FrontEnd:
                 self.stdscr.touchwin()
                 self.stdscr.refresh()
             elif c == ord('l'):
-                self.list_directory()
+                self.list_directory(self.song_root)
 
     def update_song(self):
-        self.stdscr.addstr(15, 10, "                                        ")
+        self.stdscr.addstr(15, 10, '                                        ')
         self.stdscr.addstr(
-                15, 10, "Now playing: " + self.player.getCurrentSong())
+                15, 10, 'Now playing: ' + self.player.getCurrentSong())
 
     def play(self, song):
         self.player.play(song)
@@ -46,7 +47,7 @@ class FrontEnd:
     def change_song(self):
         changeWindow = curses.newwin(5, 40, 5, 50)
         changeWindow.border()
-        changeWindow.addstr(0, 0, "What is the file path?", curses.A_REVERSE)
+        changeWindow.addstr(0, 0, 'What is the song name?', curses.A_REVERSE)
         self.stdscr.refresh()
         curses.echo()
         path = changeWindow.getstr(1, 1, 30)
@@ -55,26 +56,32 @@ class FrontEnd:
         self.stdscr.touchwin()
         self.stdscr.refresh()
         self.player.stop()
-        self.player.play(path.decode(encoding="utf-8"))
+        self.player.play(path.decode(encoding='utf-8'))
 
-    def list_directory(self):
+    def list_directory(self, path):
         '''
         Lists directories of current folder and displays
         only the files that are in the directory and stores
         them into the self. This is just a surbey of the songs,
         when selecting a song a different command is used
         '''
-        self.root_directory_files = [f for f in os.listdir('./')
-                                     if os.path.isfile(os.path.join('./', f))]
-        list_window = curses.newwin(5, 200, 5, 200)
+        self.root_directory_files = [f for f in os.listdir(path)
+                                     if os.path.isfile(os.path.join(path, f))]
+        list_window = curses.newwin(
+                len(self.root_directory_files) + 5, 200, 5, 100)
         list_window.border()
         list_window.addstr(0,
                            0,
                            'Songs in current directory',
                            curses.A_REVERSE)
+        self.stdscr.refresh()
+        curses.noecho()
         for idx, val in enumerate(self.root_directory_files):
             list_window.addstr(idx + 1, 10, val)
-        self.stdscr.refresh()
+
+        list_window.addstr(
+                len(self.root_directory_files) + 3, 10, 'Press ENTER to exit')
+        exit = list_window.getstr(1, 1, 30)
         curses.noecho()
         del list_window
         self.stdscr.touchwin()
