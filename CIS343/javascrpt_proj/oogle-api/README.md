efcdc3a9455daf77f89ace5235636b17c50f2aec
Jarred Parr <jparr721@gmail.com>
2018-11-25 12:22:05 -0500

    add readme and n_queens project

---
+++ CIS343/javascrpt_proj/oogle-api/README.md
@@ -0,0 +1,14 @@
+# Oggle-Api
+This is the starter code for the api system.
+
+## Using this system
+### Compiling
+If you're using typescript then you'll need to set up your own build seuquence, however, I personally find `ts-node` to be easy to use, or just exporting the module
+
+### Running (js)
+Run steps are simple, and as follows:
+```
+npm install
+npm start
+```
+That's all there is to it! Provided you have the package files you should be good to go.
---
+++ CIS677/n_queens/n_queens.cc
@@ -0,0 +1,95 @@
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+template <unsigned n>
+class NQueens {
+  public:
+    /**
+     * Prints the game board
+     */
+    void print(const std::vector<std::vector<int>> game_board) {
+      for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < n; ++j) {
+          std::cout << game_board[i][j] << " " << std::flush;
+        }
+        std::cout << std::endl;
+      }
+    }
+
+    /**
+     * Sequential runs until all n queens
+     * have been placed
+     */
+    auto sequential() {
+      if constexpr (n == 0) {
+        return false;
+      }
+      // Start with 2d vector of zeros with game board dims
+      std::vector<std::vector<int>> game_board(n, std::vector<int>(n, 0));
+      if (!sequential_helper(game_board, 0)) {
+        std::cout << "No solutions." << std::endl;
+        return EXIT_SUCCESS;
+      }
+
+      print(game_board);
+    }
+
+    bool valid_spot(const std::vector<std::vector<int>>& game_board, int row, int col) const {
+      // Check the row
+      for (int i = 0; i < col; ++i) {
+        if (game_board[row][i])
+          return false;
+      }
+
+      // Check left diagonal
+      for (int i = 0; i < col; ++i) {
+        if (game_board[i][col - 1 - i])
+          return false;
+      }
+
+      // Check right diagonal
+      for (int i = 0; i < col; ++i) {
+        if (game_board[i][i])
+          return false;
+      }
+
+      return true;
+    }
+
+    bool sequential_helper(std::vector<std::vector<int>>& game_board, int col) {
+      if (col >= n) {
+        return true;
+      }
+
+      for (int i = 0; i < n; ++i) {
+        if (valid_spot(game_board, i, col)) {
+          game_board[i][col] = 1;
+
+          // Recurse!
+          if (sequential_helper(game_board, col + 1))
+            return true;
+
+          // Backtrack if this doesn't work
+          game_board[i][col] = 0;
+        }
+      }
+
+      return false;
+    }
+
+    /**
+     * Parallel runs in MPI until all
+     * queens have been placed
+     */
+    void parallel() {
+      // TODO
+    }
+};
+
+int main() {
+  NQueens<8> nq;
+  nq.sequential();
+
+  return EXIT_SUCCESS;
+}
