f28a9f5b5f1c9365c8894c04b799eb26fc1d5deb
Jarred Parr <jparr721@gmail.com>
2018-11-27 18:05:07 -0500

    add faster solution

--- CIS677/n_queens/n_queens.cc
+++ CIS677/n_queens/n_queens.cc
@@ -1,5 +1,8 @@
 #include <cstdint>
+#include <cmath>
 #include <iostream>
+#include <memory>
+#include <mpi.h>
 #include <vector>
 
 template <unsigned n>
@@ -9,70 +12,49 @@ class NQueens {
      * Prints the game board
      */
     void print(const std::vector<std::vector<int>> game_board) {
-      for (int i = 0; i < n; ++i) {
-        for (int j = 0; j < n; ++j) {
+      for (auto i = 0u; i < n; ++i) {
+        for (auto j = 0u; j < n; ++j) {
           std::cout << game_board[i][j] << " " << std::flush;
         }
         std::cout << std::endl;
       }
     }
 
+
     /**
      * Sequential runs until all n queens
      * have been placed
      */
     auto sequential() {
-      if constexpr (n == 0) {
-        return false;
-      }
-      // Start with 2d vector of zeros with game board dims
-      std::vector<std::vector<int>> game_board(n, std::vector<int>(n, 0));
-      if (!sequential_helper(game_board, 0)) {
-        std::cout << "No solutions." << std::endl;
-        return EXIT_SUCCESS;
-      }
+      std::vector<std::vector<std::string>> valid;
+      std::vector<int> queens;
+      sequential_helper(valid, queens);
 
-      print(game_board);
-      return EXIT_SUCCESS;
+      return valid.size();
     }
 
-    bool valid_spot(const std::vector<std::vector<int>>& game_board, int row, int col) const {
-      for (int i = 0; i < col; ++i) {
-        if (game_board[row][i])
-          return false;
-
-        for (int i = row, j = col; i >= 0 && j >=0; --i, --j) {
-          if (game_board[i][j])
-            return false;
+    void sequential_helper(std::vector<std::vector<std::string>>& done, std::vector<int>& queens) {
+      if (queens.size() == n) {
+        done.emplace_back();
+        for (auto i = 0u; i < n; ++i) {
+          done.back().emplace_back(n, '.');
+          done.back().back()[queens[i]] = 'Q';
         }
+      } else {
+        for (auto i = 0u; i < n; ++i) {
+          bool valid = true;
 
-        for (int i = row, j = col; j >= 0 && i < n; ++i, --j) {
-          if (game_board[i][j])
-            return false;
-        }
-      }
-      return true;
-    }
+          for (auto j = 0u; valid && j < queens.size(); ++j) {
+            valid = (queens[j] != i) && (abs(queens[j] - i) != queens.size() - j);
+          }
 
-    bool sequential_helper(std::vector<std::vector<int>>& game_board, int col) {
-      if (col >= n) {
-        return true;
-      }
-
-      for (int i = 0; i < n; ++i) {
-        if (valid_spot(game_board, i, col)) {
-          game_board[i][col] = 1;
-
-          // Recurse!
-          if (sequential_helper(game_board, col + 1))
-            return true;
-
-          // Backtrack if this doesn't work
-          game_board[i][col] = 0;
+          if (valid) {
+            queens.push_back(i);
+            sequential_helper(done, queens);
+            queens.pop_back();
+          }
         }
       }
-
-      return false;
     }
 
     /**
@@ -80,13 +62,46 @@ class NQueens {
      * queens have been placed
      */
     void parallel() {
-      // TODO
+      MPI_Request req;
+      int rank, num_nodes;
+
+      MPI_Init(nullptr, nullptr);
+      MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+      MPI_Comm_size(MPI_COMM_WORLD, &num_nodes);
+
+      std::shared_ptr<int> return_values(new int[2 * n]);
+      int data_size, machine = 0;
+
+      if (rank == this->MASTER) {
+
+      } else {
+        for (;;) {
+          MPI_Isend(&machine, 1, MPI_INT, this->MASTER, this->TAG, MPI_COMM_WORLD, &req);
+          MPI_Wait(&req, MPI_STATUS_IGNORE);
+
+          MPI_Recv(&data_size, 1, MPI_INT, this->MASTER, this->TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+          if (data_size < 0) {
+            break;
+          }
+
+          MPI_Recv(reinterpret_cast<void*>(&return_values), data_size * 2, MPI_INT, this->MASTER, this->TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+          this->sequential();
+        }
+      }
+
+      MPI_Finalize();
     }
+
+  private:
+    const int MASTER = 0;
+    const int TAG = 0;
 };
 
-int main() {
-  NQueens<8> nq;
-  nq.sequential();
+int main(int argc, char** argv) {
+  NQueens<11> nq;
+  auto value = nq.sequential();
+  std::cout << value << std::endl;
 
   return EXIT_SUCCESS;
 }
