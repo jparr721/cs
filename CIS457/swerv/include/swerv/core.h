dc58ad4019fe0e1f9fa6fda7a8cd24d939682bc3
Jarred Parr <jparr721@gmail.com>
2018-12-01 20:45:54 -0500

    almost done

--- CIS457/swerv/include/swerv/core.h
+++ CIS457/swerv/include/swerv/core.h
@@ -47,15 +47,48 @@ namespace swerver {
           </body>
           </html>
         )";
+      const std::string html304 =
+        R"(
+          <!DOCTYPE html>
+          <html>
+          <title>Not Modified</title>
+          <body>
+          <h1>
+          File not modified.
+          </h1>
+          </body>
+          </html>
+        )";
+      const std::string html200Failed =
+        R"(
+          <!DOCTYPE html>
+          <html>
+          <title>Error</title>
+          <body>
+          <h1>
+          Failed to read file, sorry
+          </h1>
+          </body>
+          </html>
+        )";
+      const std::string html200Default =
+        R"(
+          <!DOCTYPE html>
+          <html>
+          <title>Howdy</title>
+          <body>
+          <h1>
+          What's up, dawg?
+          </h1>
+          </body>
+          </html>
+        )";
       std::string docroot = ".doc";
       std::string logfile = ".log";
-      int port = 1024;
+      int port = 3000;
 
       static void* thread_handler(void* args);
-
-      bool handle_args(int argc, char** argv);
       void usage() const;
-
       void send_http_response(
           int socket,
           int code,
@@ -64,8 +97,17 @@ namespace swerver {
           std::string filename,
           std::string last_modified,
           std::string file_data) const;
+      void log_request(std::string log);
 
-      template <typename T>
-      auto read_file(std::string filename) const;
+      std::string check_file_mod(std::string path);
+      std::string get_current_time();
+      std::string modded_since(std::string req, std::string filename, std::string ext);
+      std::string read_file(std::string filename) const;
+      std::string make_path(std::string dir, std::string file) const;
+
+      bool init_system_file(std::string path, std::string default_path);
+      bool handle_args(int argc, char** argv);
+
+      Core::ContentType make_content_type(std::string ext);
   };
 } // namespace swerver
--- CIS457/swerv/src/core.cc
+++ CIS457/swerv/src/core.cc
@@ -1,7 +1,10 @@
 #include <swerv/core.h>
 
+#include <boost/algorithm/string.hpp>
 #include <chrono>
+#include <cstdlib>
 #include <cstring>
+#include <filesystem>
 #include <fstream>
 #include <iomanip>
 #include <iostream>
@@ -10,25 +13,43 @@
 #include <pthread.h>
 #include <sstream>
 #include <sys/socket.h>
+#include <sys/stat.h>
+#include <unistd.h>
 
 namespace swerver {
-  template <typename T>
-  auto Core::read_file(std::string filename) const {
-    T value;
-    std::ifstream f(filename);
-    if (!f.good()) value = false;
-
-    if constexpr (!value) {
-      return false;
+  Core::ContentType Core::make_content_type(std::string ext) {
+    if (ext == "html") {
+      return Core::ContentType::html;
+    } else if (ext == "txt") {
+      return Core::ContentType::text;
+    } else if (ext == "jpg") {
+      return Core::ContentType::jpeg;
+    } else if (ext == "pdf") {
+      return Core::ContentType::pdf;
+    } else {
+      return Core::ContentType::html;
     }
+  }
+
+  void Core::log_request(std::string log) {
+    std::cout << log << std::endl;
+  }
+
+  std::string Core::make_path(std::string dir, std::string file) const {
+    std::filesystem::path p = std::filesystem::current_path();
+
+    return std::string(p);
+  }
+
+  std::string Core::read_file(std::string filename) const {
+    std::ifstream f(filename);
+    if (!f.good()) return "";
 
     // Write to a string
-    std::string str((std::istreambuf_iterator<char>(f),
-                     std::istreambuf_iterator<char>()));
-    value = str;
+    std::string str((std::istreambuf_iterator<char>(f)),
+                     std::istreambuf_iterator<char>());
 
-
-    return value;
+    return str;
   }
 
   void Core::usage() const {
@@ -36,43 +57,86 @@ namespace swerver {
       "Usage:\n"
       " swerver -p <PORT>       Specifies which port to run on.\n"
       " swerver -docroot <DIR>  Specifies where the docroot will be.\n"
-      " swerver -logfile <FILE> Specifies where log files will be written to.\n";
+      " swerver -logfile <FILE> Specifies where log files will be written to.\n"
       " swerver default         Run server with default settings.\n";
 
     std::cout << help << std::endl;
   }
 
+  std::string Core::check_file_mod(std::string path) {
+    auto time = std::filesystem::last_write_time(path);
+    auto cftime = decltype(time)::clock::to_time_t(time);
+    auto final_time = std::asctime(std::localtime(&cftime));
+    return std::string(final_time);
+  }
+
+  std::string Core::get_current_time() {
+    auto now = std::chrono::system_clock::now();
+    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+    auto current_time = std::put_time(std::localtime(&in_time_t), "%Y-%m-%d-%X");
+    std::stringstream time;
+    time << current_time;
+
+    return time.str();
+  }
+
+  std::string Core::modded_since(std::string req, std::string filename, std::string ext) {
+    if (ext == "html" || ext == "txt") {
+      std::string mod_header;
+      // Need to double check this
+      mod_header = req.substr(0, 19);
+
+      if (mod_header == "If-Modified-Since:") {
+        std::string date_mod_since = req.substr(19, 29);
+        std::string file_mod = check_file_mod(filename);
+
+        if (date_mod_since == file_mod) {
+          return file_mod;
+        }
+        return file_mod;
+      }
+    }
+
+    return "";
+  }
+
   bool Core::handle_args(int argc, char** argv) {
-    if (argc < 3) {
+    if (argc < 2) {
       usage();
       return false;
     }
-    for (int i = 1; i < argc - 1; ++i) {
-      std::string opt(argv[i]);
-      if (opt == "-p") {
-         this->port = std::stoi(argv[i + 1]);
-      } else if (opt == "-docroot") {
-         this->docroot = std::string(argv[i + 1]);
-      } else if (opt == "-logfile") {
-         this->logfile = std::string(argv[i+ 1]);
-      } else {
-         std::cerr << "Error, argument not supported" << std::endl;
-         return false;
+
+    if (std::string(argv[1]) == "default") {
+      std::cout << "Running server with default configuration" << std::endl;
+      return true;
+    } else {
+      for (int i = 1; i < argc - 1; ++i) {
+        std::string opt(argv[i]);
+        if (opt == "-p") {
+           this->port = std::stoi(argv[i + 1]);
+        } else if (opt == "-docroot") {
+           this->docroot = std::string(argv[i + 1]);
+        } else if (opt == "-logfile") {
+           this->logfile = std::string(argv[i + 1]);
+        } else {
+           std::cerr << "Error, argument not supported" << std::endl;
+           return false;
+        }
       }
     }
 
     return true;
   }
 
-    void Core::send_http_response(
-        int socket,
-        int code,
-        bool keep_alive,
-        Core::ContentType content_type,
-        std::string filename,
-        std::string last_modified,
-        std::string file_data
-        ) const {
+  void Core::send_http_response(
+      int socket,
+      int code,
+      bool keep_alive,
+      Core::ContentType content_type,
+      std::string filename,
+      std::string last_modified,
+      std::string file_data
+      ) const {
     std::string code_msg, connection_type, content_type_string, pdf_header;
     auto now = std::chrono::system_clock::now();
     auto in_time_t = std::chrono::system_clock::to_time_t(now);
@@ -154,6 +218,36 @@ namespace swerver {
     std::cout << "{ Header: " << header.str() << "}" << std::endl;
   }
 
+  bool Core::init_system_file(std::string path, std::string default_path) {
+    struct stat info;
+
+    if (stat(path.c_str(), &info) != 0) {
+      std::cout << "Failed to find directory: " << path << " creating" << std::endl;
+
+      const int dir_error = mkdir(path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
+      if (dir_error < 0) {
+        std::cout << "Failed to create directory with name: " << path << " creating with default name: " << default_path << std::endl;
+      }
+
+      if (stat(path.c_str(), &info) != 0) {
+        std::cout << "Failed to find " << default_path << " creating now..." << std::endl;
+        const int dir_error = mkdir(default_path.c_str(), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
+        if (dir_error < 0) {
+          std::cout << "Failed to create default directory or find it, check your directory path and try again. Exiting" << std::endl;
+          return false;
+        }
+
+        // Reset our globals to the defaults
+        if (default_path == ".doc") this->docroot = default_path;
+        else if (default_path == ".log") this->logfile = default_path;
+      }
+    }
+
+    return true;
+  }
+
   void* Core::thread_handler(void* args) {
     Core::thread t;
     Core c;
@@ -165,7 +259,39 @@ namespace swerver {
       int in = recv(t.socket, line, 5000, 0);
       if (in < 0) {
         std::cout << "Failed to get data from client" << std::endl;
-        /* c.send_http_response(404); */
+      }
+
+      std::string input(line);
+
+      c.log_request(input);
+
+      std::string request_type = input.substr(0, 3);
+
+      if (request_type == "GET") {
+        bool file_request = false;
+        std::string req = input.substr(5, input.substr(5).find(" ", 0));
+
+        if (req == "") {
+          c.send_http_response(t.socket, 200, true, c.Core::ContentType::html, "", "", c.html200Default);
+        } else {
+          std::vector<std::string> filename;
+          auto content_type = c.make_content_type(filename[1]);
+          boost::algorithm::split(filename, req, boost::is_any_of("."));
+
+          // Check if the file exists first
+          if (access(req.c_str(), F_OK) != -1) {
+            std::string did_mod = c.modded_since(input, req, filename[1]);
+            if (did_mod != "") {
+              auto file_contents = c.read_file(req);
+              if (file_contents == "") c.send_http_response(t.socket, 200, true, c.Core::ContentType::html, "", "", c.html200Failed);
+              c.send_http_response(t.socket, 200, true, content_type, req, did_mod, file_contents);
+            } else {
+              auto file_contents = c.read_file(req);
+              if (file_contents == "") c.send_http_response(t.socket, 200, true, c.Core::ContentType::html, "", "", c.html200Failed);
+              c.send_http_response(t.socket, 304, true, content_type, req, did_mod, file_contents);
+            }
+          }
+        }
       }
     }
   }
@@ -174,6 +300,15 @@ namespace swerver {
     if (!this->handle_args(argc, argv)) {
       return EXIT_FAILURE;
     }
+
+    bool err;
+    // Create docroot
+    err = this->init_system_file(this->docroot, ".doc");
+    if (!err) return EXIT_FAILURE;
+
+    // Create logfile root
+    err = this->init_system_file(this->logfile, ".log");
+    if (!err) return EXIT_FAILURE;
 
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
 
