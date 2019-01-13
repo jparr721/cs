6199c590263c7c2c616c019dcfdf77ca7f8f5caa
unknown <fountaia@mail.gvsu.edu>
2019-01-12 19:11:37 -0500

    Working on movement of hlt bot

--- CIS365/halite/destructor.py
+++ CIS365/halite/destructor.py
@@ -55,9 +55,95 @@ class Annihilator(object):
                 cardinal_direction = (x_i, y_i)
                 # IMPLEMENT DICT VALUE CHECK HERE
 
+game.ready("rekbot")
+logging.info("Player ID is {}.".format(game.my_id))
 
 while True:
     BUTTHOLE_SHREDDER = Annihilator()
+
+    me = game.me
+    game_map = game.game_map
+
+    # A command queue holds all the commands you will run this turn. You build this list up and submit it at the
+    #   end of the turn.
+    command_queue = []
+    ship_status = {}
+    ## SHIP LOCATIONS INDEXED BY [[x, y], ship_id]
+    ship_locations = {}
+
+    for ship in me.get_ships():
+        x, y = ship.position.x, ship.position.y
+        logging.info("Ship {} has {} halite.".format(ship.id, ship.halite_amount))
+
+        if ship.id not in ship_status:
+            ship_status[ship.id] = "exploring"
+
+    ##This splits the ships into two random groups.  Group true searches east and false searches west.
+        if ship.group not True or not False:
+            z = random.randint(1,2)
+            if z = 1
+                ship.group = True
+            else:
+                ship.group = False
+
+        if ship_status[ship.id] == "returning":
+            if ship.position == me.shipyard.position:
+                ship_status[ship.id] = "exploring"
+            else:
+                move = game_map.naive_navigate(ship, find_drop(me.shipyard.position, me.get_dropoffs())
+                comand_queue.append(ship.move(move))
+
+        elif ship.halite_amount >= constants.MAX_HALITE / 4:
+            ship_status[ship.id] = "returning"
+
+        if ship.is_full:
+            move = game_map.naive_navigate(ship, find_drop(me.shipyard.position, me.get_dropoffs())
+            comand_queue.append(ship.move(move))
+            ship_status[ship_id] = "returning"
+
+    ##Currently this is what determins which way the ship moves.  Would like to improve it to spawn drop off's
+    # if it's far enough from another dropoff and has enough hlt.
+        if ship.group == True:
+            if not game_map[position.directional_offset(West)].is_occupied:
+                command_queue.append(ship.moveDirection.West)
+            if not game_map[position.directional_offset(North)].is_occupied:
+                command_queue.append(ship.moveDirection.North)
+            if not game_map[position.directional_offset(South)].is_occupied:
+                command_queue.append(ship.moveDirection.South)
+            else:
+                command_queue.append(ship.stay_still())
+
+         if ship.group == False:
+            if not game_map[position.directional_offset(East)].is_occupied:
+                command_queue.append(ship.moveDirection.East)
+            if not game_map[position.directional_offset(South)].is_occupied:
+                command_queue.append(ship.moveDirection.South)
+            if not game_map[position.directional_offset(North)].is_occupied:
+                command_queue.append(ship.moveDirection.North)
+            else:
+                command_queue.append(ship.stay_still())
+
+    # If the game is in the first 300 turns and you have enough halite, spawn a ship.
+    # Don't spawn a ship if you currently have a ship at port, though - the ships will collide.
+    if game.turn_number <= 300 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
+        command_queue.append(me.shipyard.spawn())
+
+    # Send your moves back to the game environment, ending this turn.
+    game.end_turn(command_queue)
+
+
+    ship.position.directional_offset(test_direction)
+    position.get_surrounding_cardinals()
+    game_map[position].halite_amount
+
+
+
+
+
+
+
+
+    
 
 ##!/usr/bin/env python3
 ## Python 3.6
