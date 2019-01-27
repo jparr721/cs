d9342edcce1af437faa977484292b71db5ec8568
Jarred Parr <jparr721@gmail.com>
2019-01-27 12:58:43 -0500

    fix compilation, preparing to train

--- CIS365/halite/run.sh
+++ CIS365/halite/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-./halite --replay-directory replays/ -vvv --width 32 --height 32 "python3.6 MyBot.py 1 y" "python3.6 dummy.py"
+./halite --replay-directory replays/ -vvv --width 32 --height 32 "python MyBot.py 1 y" "python dummy.py"
