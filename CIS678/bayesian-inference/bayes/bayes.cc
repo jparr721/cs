1cfe2c111af2c17bfa4578906d462906a1eae84b
Jarred Parr <jparr721@gmail.com>
2019-02-08 21:53:26 -0500

    add data reading

--- CIS678/bayesian-inference/bayes/bayes.cc
+++ CIS678/bayesian-inference/bayes/bayes.cc
@@ -1,5 +1,8 @@
+#include <algorithm>
 #include <fstream>
 #include <iostream>
+#include <iterator>
+#include <numeric>
 #include <omp.h>
 #include <stdexcept>
 #include <sstream>
@@ -8,6 +11,8 @@
 namespace bayes {
   document::document(const std::string& doc_path) {
     load_document(doc_path);
+    stem_document();
+
   }
 
   void document::load_document(const std::string& doc_path) {
@@ -32,24 +37,48 @@ namespace bayes {
         } else {
           topic_frequencies_[topic] = 0;
         }
-        for (size_t i = 0; i < words.size(); ++i) {
+        for (size_t j = 0; j < words.size(); ++j) {
           for (const auto& suffix : suffixes) {
-            if (ends_with(words[i], suffix)) {
-              words[i].substr(0, words[i].size() - suffix.size());
+            if (ends_with(words[j], suffix)) {
+              words[j].substr(0, words[j].size() - suffix.size());
             }
           }
         }
+        // Remove the topic and add it to frequencies
+        count_word_frequencies(words);
+        lines_[i] = join(words, " ");
+      }
+    }
+
+    // Clean up word frequencies outside of parallel region
+    for (auto it = topics.begin(); it != topics.end(); ++it) {
+      if (word_frequencies_[it]) {
+        word_frequencies_.erase(it);
       }
     }
   }
 
-  void document::lemmatize_document() {
-
+  void document::count_word_frequencies(const std::vector<std::string>& words) {
+    for (const auto& word : words) {
+      if (word_frequencies_.find(word) != word_frequencies_.end()) {
+        ++word_frequencies_[word];
+      } else {
+        word_frequencies_[word] = 0;
+      }
+    }
   }
 
   bool document::ends_with(const std::string& word, const std::string& suffix) {
     return word.size() > suffix.size() &&
       word.substr(word.size() - suffix.size()) == suffix;
+  }
+
+  std::string document::join(const std::vector<std::string>& words, const std::string& delimiter) const {
+    std::ostringstream oss;
+    std::copy(words.begin(), words.end() - 1, std::ostream_iterator<std::string>(oss, delimiter.c_str()));
+    oss << words.back();
+
+    return oss.str();
   }
 
   std::vector<std::string> document::split(std::string line) {
--- CIS678/bayesian-inference/bayes/bayes.h
+++ CIS678/bayesian-inference/bayes/bayes.h
@@ -16,11 +16,13 @@ namespace bayes {
 
     void load_document(const std::string& doc_path);
     void stem_document();
-    void lemmatize_document();
+    void count_word_frequencies(const std::vector<std::string>& words);
 
-    int word_count(const std::string& word) const;
+    int get_word_count(const std::string& word) const;
 
     bool ends_with(const std::string& word, const std::string& suffix);
+
+    std::string join(const std::vector<std::string>& words, const std::string& delimiter) const;
 
     std::vector<std::string> split(std::string line);
 
@@ -28,6 +30,14 @@ namespace bayes {
     frequency_map topic_frequencies_;
     frequency_map word_frequencies_;
     const std::array<std::string, 3> suffixes{{"ed", "ing", "'s"}};
+    const std::array<std::string, 20> topics{
+      {
+        "atheism", "graphics", "mswindows", "pc", "mac", "xwindows",
+        "forsale", "autos", "motorcycles", "baseball", "hockey",
+        "cryptology", "electronics", "medicine", "space", "christianity"
+        "guns", "mideastpolitics", "politics", "religion"
+      }
+    };
   };
 
   class Bayes {
