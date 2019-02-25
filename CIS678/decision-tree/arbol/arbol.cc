a67b182f146a19abdc98d18885915fb34026f603
Jarred Parr <jparr721@gmail.com>
2019-02-25 15:19:26 -0500

    good progress so far..

--- CIS678/decision-tree/arbol/arbol.cc
+++ CIS678/decision-tree/arbol/arbol.cc
@@ -4,8 +4,12 @@
 #include "arbol.h"
 
 namespace arbol {
-  double gain(const double entropy) {
-    return 0.0;
+  double sum_attributes(const std::string& k) {
+    auto val_opts = value_ranges[k];
+  }
+
+  double Arbol::gain(const double S, const double a) {
+    return S - a;
   }
 
   double Arbol::entropy(const std::string& k) {
@@ -13,9 +17,39 @@ namespace arbol {
     entropies_[k] = -probability * std::log2(probability);
     return 0.0;
   }
+
+  void Arbol::fit(const std::vector<std::vector<std::string>> data) {
+    const std::vector<std::string> labels = data_[0];
+    auto class_label_name = labels[labels.size() - 1];
+    auto total_entries_count = data.size();
+
+    // First, we want to get the overall probability of each class label
+    for (uint i = 1; i < data.size(); ++i) {
+      ++class_probabilities_[data[i][labels.size() - 1]];
+    }
+
+    // Now, get our true probabilities of each label
+    for (const auto& proba : class_probabilities_) {
+      proba->second = (double)proba->second / (double)total_entries_count;
+    }
+
+    // Now, calculate the entropy of our labels
+    double S{0.0};
+
+    auto keys = util::extract_keys<std::string, double>(class_probabilities_);
+    S = entropy(keys[0]);
+
+    // Now we sum the rest into S.
+    for (int i = 1; i < keys.size(); ++i) { S -= entropy(keys[i]); }
+
+    // With S as a now static value, we can move on calculating mad gains
+  }
+
 } // namespace arbol
 
 int main(int argc, char** argv) {
-  auto csv = arbol::util::load_csv(std::string(argv[1]));
+  std::string arg = argv[1];
+  auto csv = arbol::util::load_non_numeric(arg);
+  arbol::util::print_vector(csv);
   return EXIT_SUCCESS;
 }
--- CIS678/decision-tree/arbol/arbol.h
+++ CIS678/decision-tree/arbol/arbol.h
@@ -1,5 +1,6 @@
 #include <Eigen/Dense>
 #include <fstream>
+#include <map>
 #include <string>
 #include <vector>
 #include <unordered_map>
@@ -28,18 +29,70 @@ namespace util {
                M::ColsAtCompileTime,
                Eigen::RowMajor>>(values.data(), rows, values.size()/rows);
   }
+
+  template<typename TK, typename TV>
+  std::vector<TK> extract_keys(std::unordered_map<TK, TV> const& input_map) {
+    std::vector<TK> retval;
+    for (auto const& element : input_map) {
+      retval.push_back(element.first);
+    }
+    return retval;
+  }
+
+  template<typename TK, typename TV>
+  std::vector<TV> extract_values(std::unordered_map<TK, TV> const& input_map) const {
+    std::vector<TV> retval;
+    for (auto const& element : input_map) {
+      retval.push_back(element.second);
+    }
+    return retval;
+  }
+
+
+  std::vector<std::vector<std::string>> load_non_numeric(const std::string& path) {
+    std::ifstream indata;
+    indata.open(path);
+
+    std::vector<std::vector<std::string>> values;
+    std::string line;
+    while (std::getline(indata, line)) {
+      std::vector<std::string> line_data;
+      std::stringstream ss(line);
+      std::string cell;
+      while (std::getline(ss, cell, ',')) {
+        line_data.push_back(cell);
+      }
+
+      values.push_back(line_data);
+    }
+
+    return values;
+  }
+
+  void print_vector(const std::vector<std::vector<std::string>>& the_vector) {
+    for (const auto& row : the_vector) {
+      for (const auto& col : row) {
+        std::cout << col << ", ";
+      }
+
+      std::cout << std::endl;
+    }
+  }
 } // namespace util
   using probability_map =
     std::unordered_map<std::string, double>;
 
 class Arbol {
   public:
-    double entropy(const std::string& k);
-    double gain(const double entropy);
-  private:
+    Arbol(const std::map<std::string, std::vector<std::string>> value_ranges) : value_ranges_(value_ranges) {};
 
-    Eigen::MatrixXd data_;
+    void fit();
+  private:
+    double entropy(const std::string& k);
+    double gain(const double S, const double a);
+
     probability_map class_probabilities_;
     probability_map entropies_;
+    std::map<std::string, std::vector<std::string>> value_ranges_;
 };
 } // namespace arbol
