b88ba767c0eae2ed89f3f5b42218f61ad7f4deba
Jarred Parr <jparr721@gmail.com>
2019-03-20 14:50:20 -0400

    dammit alex

--- CIS452/lab7/count.c
+++ CIS452/lab7/count.c
@@ -8,15 +8,17 @@ int main ()
 {
 	long int count = 1;
 	//Initialize Semaphore here
-	int sem = semget(IPC_PRIVATE, 1, 00600);
+	int sem = 0;
+	key_t key = ftok("!", 3);
+	
+	if ((sem = semget(key, 1, IPC_CREAT | 0666)) == -1) {
+		perror("broke");
+		return EXIT_FAILURE;
+	}
 
 	for(;;) {
 		//Increment semaphore count here		
-
-		if(semctl(sem, 0, SETVAL, count) == -1){
-			perror("Darn\n");
-			exit(1);
-		}
+		count = semctl(sem, 0, GETVAL);
 
 		count++;
 
